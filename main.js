a5a554863c25051b237eeb96f6a04e088c958de5
Chandan0424 <76838260+Chandan0424@users.noreply.github.com>
2021-10-26 16:49:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+noseX=0;
+noseY=0;
+
 function preload(){
 
 }
@@ -7,6 +10,8 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO)
     video.hide();
+    posenet=ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotPoses);
 }
 
 function draw(){
@@ -15,4 +20,16 @@ function draw(){
 
 function takeSnapShot(){
     save('mustache.png');
+}
+
+function modelLoaded(){
+    console.log("Model is Loaded!!!!:):o");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+    }
 }
